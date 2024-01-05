43f4a3858d810e21126780b2fc81a1530ced822e
Adam Eldridge <ae@Adam-PC.localdomain>
2024-01-05 18:05:44 +0000

    finished tutorial

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+
+
+
 
 function App() {
   return (
@@ -9,12 +17,13 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact />
+          <Route path='/' exact element={ <Home />}></Route>
+          <Route path='/services' exact element={ <Services />}></Route>
+          <Route path='/products' exact element={ <Products />}></Route>
+          <Route path='/sign-up' exact element={ <SignUp />}></Route>
         </Routes>
       </Router>
     </>
-      
-
   );
 }
 
